de6df8f75696af0f67b838966f99ad614f979e88
Eric <erstaples@gmail.com>
2017-05-05 14:59:00 -0700

    wrap timestamp in single quotes to prevent it from being converted to sci. notation in the release

--- cmd/release.go
+++ cmd/release.go
@@ -137,7 +137,7 @@ var releaseCmd = &cobra.Command{
 		//build helm cmd
 		//add standard values to be made available in the helm releases
 		timestamp := strconv.FormatInt(time.Now().UTC().Unix(), 10)
-		setValues := "environment=" + env + ",packageId=" + packageId + ",timestamp=" + timestamp
+		setValues := "environment=" + env + ",packageId=" + packageId + ",timestamp='" + timestamp + "'"
 		if xdebug {
 			setValues += ",xdebugHost=" + xdebugHost
 		}
